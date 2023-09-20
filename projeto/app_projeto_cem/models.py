c31ffa7f1fbe74df9da61e6aae7e270fc1ab4b7c
Guilherme Leo de Oliveira <go507744@gmail.com>
2023-09-20 00:57:16 -0300

    iniciei a parte de cadastro de usuarios

--- projeto/app_projeto_cem/models.py
+++ projeto/app_projeto_cem/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+#Parte de Banco de dados, aqui consigo criar tabelas por codigos python e o django traduz em sql
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    idade = models.IntegerField()
--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_projeto_cem', #comando nescessário para rodar a aplicação
 ]
 
 MIDDLEWARE = [
