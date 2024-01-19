2edcb8ce3f24ffa42c1c11a24091e0d85efdb1c6
Guilherme Leo de Oliveira <go507744@gmail.com>
2024-01-19 12:23:36 -0300

    adicionando precadtecnico as urls

--- administracao/admin.py
+++ administracao/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Adm, Tecnico
+from .models import Adm, Tecnico, preCadTecnico
 # Register your models here.
 admin.site.register(Adm)
 admin.site.register(Tecnico)
+admin.site.register(preCadTecnico)
